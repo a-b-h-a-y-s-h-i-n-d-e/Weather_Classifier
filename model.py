4dd543c85ee17684f4843c0f703ad1ba8da843aa
a-b-h-a-y-s-h-i-n-d-e <theabhay.shinde@gmail.com>
2024-07-18 13:19:11 +0530

    cnn class

---
+++ model.py
@@ -0,0 +1,33 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class WeatherCNN(nn.Module):
+
+    def __init__(self):
+        super(WeatherCNN, self).__init__()
+
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+
+        self.fc1 = nn.Linear(64 * 32 * 32, 256)
+        self.dropout1 = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(256, 128)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc3 = nn.Linear(128, 4)
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+
+        x= x.view(-1, 64 * 32 * 32)
+        x = F.relu(self.fc1(x))
+        x = self.dropout1(x)
+        x = self.fc2(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        return x
+
+
